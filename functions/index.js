08de36eb07c5c4e084bce4edd1eadb83d1fdf5df
cdalangin <florenzdalangin@gmail.com>
2020-11-07 22:22:41 -0500

    try deploy

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,12 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin: true }));
 
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + "/public"));
+
 var serviceAccount = require("./permissions.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -13,62 +19,67 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-
-
 // create
 app.post('/api/create', (req, res) => {
-    (async () => {
-        try {
-          await db.collection('items').doc('/' + req.body.id + '/')
-              .create({item: req.body.item});
-          return res.status(200).send();
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send(error);
-        }
-      })();
-  });
- 
+  (async () => {
+    try {
+      await db.collection('items').doc('/' + req.body.id + '/')
+        .create({ item: req.body.item });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
 
 //Creating Nodemailer transporter using your Mailtrap SMTP details
 let transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-  port: 2525,  
+  host: "smtp.mailtrap.io",
+  port: 2525,
   auth: {
     user: "336bded6d33bec",
     pass: "e833d9b254bb4a"
   }
 });
- 
+
 //Creating a Firebase Cloud Function
-exports.emailSender = functions.https.onRequest((req, res) => {   
-      
-            //Defining mailOptions
-            const mailOptions = {
-            from: 'test@gmail.com', //Adding sender's email
-            to: req.query.dest, //Getting recipient's email by query string
-            subject: 'Email Sent via Firebase', //Email subject
-            html: '<b>Sending emails with Firebase is easy!</b>' //Email content in HTML
-        };
-  
-        //Returning result
-        return transporter.sendMail(mailOptions, (err, info) => {
-            if(err){
-                return res.send(err.toString());
-            }
-            return res.send('Email sent succesfully');
-        });
-       
+exports.emailSender = functions.https.onRequest((req, res) => {
+
+  //Defining mailOptions
+  const mailOptions = {
+    from: 'test@gmail.com', //Adding sender's email
+    to: req.query.dest, //Getting recipient's email by query string
+    subject: 'Email Sent via Firebase', //Email subject
+    html: '<b>Sending emails with Firebase is easy!</b>' //Email content in HTML
+  };
+
+  //Returning result
+  return transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      return res.send(err.toString());
+    }
+    return res.send('Email sent succesfully');
+  });
+
 });
 
+// app.get('/hello-world', (req, res) => {
+//   return res.status(200).send('Hello World!');
+// });
 
+app.get('/', (request, response) => {
+  response.render('home.handlebars');
+});
 
+app.get('/newletter', (request, response) => {
+  response.render('newletter.handlebars');
+});
 
-
-
-
-app.get('/hello-world', (req, res) => {
-  return res.status(200).send('Hello World!');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT}`);
 });
 
 exports.app = functions.https.onRequest(app);
