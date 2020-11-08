221d0cb9d7f16315af0e706a56f63e756a268986
sijiawen <sijiaw@msn.com>
2020-11-07 16:29:19 -0800

    server

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,35 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true }));
+
+var serviceAccount = require("./permissions.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ttb2020-26890.firebaseio.com"
+});
+const db = admin.firestore();
+
+
+
+// create
+app.post('/api/create', (req, res) => {
+    (async () => {
+        try {
+          await db.collection('items').doc('/' + req.body.id + '/')
+              .create({item: req.body.item});
+          return res.status(200).send();
+        } catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+        }
+      })();
+  });
  
-//Initializing Firebase Admin SDK
-admin.initializeApp();
- 
+
 //Creating Nodemailer transporter using your Mailtrap SMTP details
 let transporter = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
@@ -20,7 +45,7 @@ exports.emailSender = functions.https.onRequest((req, res) => {
       
             //Defining mailOptions
             const mailOptions = {
-            from: 'alfo.opidi85@gmail.com', //Adding sender's email
+            from: 'test@gmail.com', //Adding sender's email
             to: req.query.dest, //Getting recipient's email by query string
             subject: 'Email Sent via Firebase', //Email subject
             html: '<b>Sending emails with Firebase is easy!</b>' //Email content in HTML
@@ -35,3 +60,15 @@ exports.emailSender = functions.https.onRequest((req, res) => {
         });
        
 });
+
+
+
+
+
+
+
+app.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!');
+});
+
+exports.app = functions.https.onRequest(app);
